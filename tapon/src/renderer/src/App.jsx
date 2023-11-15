78078f6aa6f5896d6a553aa9ae4bd9a1d6d682f6
FranciscoC <123523494+CarpinchoRecu@users.noreply.github.com>
2023-11-15 14:21:38 -0300

    Initial commit

---
+++ tapon/src/renderer/src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Home from "./components/Home.jsx"
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="*" element={<Home />} />
+        <Route path="/home" element={<Home />} key="home" />
+
+      </Routes>
+
+    </BrowserRouter>
+  )
+}
+
+export default App
