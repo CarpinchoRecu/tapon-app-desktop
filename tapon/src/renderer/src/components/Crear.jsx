bfd9be5a4f6de8591e03fb91788cd5de2fbcfccd
FranciscoC <123523494+CarpinchoRecu@users.noreply.github.com>
2023-11-17 14:24:05 -0300

    1.0.5
    creando interfaz de creador para, faltan agregar funcionalidad para conectar con base de datos

--- tapon/src/renderer/src/components/Crear.jsx
+++ tapon/src/renderer/src/components/Crear.jsx
@@ -4,13 +4,25 @@ import { MdCancel } from 'react-icons/md'
 
 const Crear = () => {
     const [abrirCreador, setAbrirCreador] = useState(false)
+    const [cantidadDeProductos, setCantidadDeProductos] = useState(0)
 
-    const handleAbrirCraedor = () => {
+    const handleAbrirCreador = () => {
         setAbrirCreador(true)
+        setCantidadDeProductos(0)
     }
 
-    const handleCerrarCraedor = () => {
+    const handleCerrarCreador = () => {
         setAbrirCreador(false)
+        setCantidadDeProductos(0)
+    }
+
+    const handleCantidadProductosChange = (event) => {
+        let value = parseInt(event.target.value, 10)
+        // Limitar la cantidad máxima de productos a 4
+        value = Math.min(value, 4)
+        // Limitar la cantidad mínima de productos a 0
+        value = Math.max(value, 0)
+        setCantidadDeProductos(value)
     }
 
     const camposCreador = [
@@ -28,51 +40,71 @@ const Crear = () => {
         },
         {
             label: 'Cantidad de Productos',
-            type: 'number'
-        },
-        {
-            label: 'Nombre Producto',
-            type: 'text'
-        },
-        {
-            label: 'Precio Producto',
-            type: 'number'
-        },
-
-        {
-            label: 'Cuotas',
-            type: 'number'
-        },
-        {
-            label: 'Cuotas Pagadas',
-            type: 'number'
-        },
-        {
-            label: 'Fecha Ultimo Pago',
-            type: 'date'
+            type: 'number',
+            value: cantidadDeProductos,
+            onChange: handleCantidadProductosChange // Manejar el cambio en la cantidad de productos
         }
     ]
 
+    const camposProducto = Array.from({ length: cantidadDeProductos }, (_, index) => ({
+        label: `Producto nº${index + 1}`,
+        campos: [
+            {
+                label: 'Nombre Producto',
+                type: 'text'
+            },
+            {
+                label: 'Precio Producto',
+                type: 'number'
+            },
+            {
+                label: 'Cuotas',
+                type: 'number'
+            },
+            {
+                label: 'Cuotas Pagadas',
+                type: 'number'
+            },
+            {
+                label: 'Fecha Ultimo Pago',
+                type: 'date'
+            }
+        ]
+    }))
+
     return (
         <>
-            <div onClick={handleAbrirCraedor} className="btn__creador__abrir">
+            <div onClick={handleAbrirCreador} className="btn__creador__abrir">
                 <p>Crear</p>
                 <LuPlusCircle />
             </div>
             {abrirCreador === true ? (
                 <div className="creador">
-                    <div onClick={handleCerrarCraedor} className="btn__creador__cerrar">
+                    <h2>Crear Clientes</h2>
+                    <section onClick={handleCerrarCreador} className="btn__creador__cerrar">
                         <MdCancel />
-                    </div>
-                    <div className="campos">
+                    </section>
+                    <section className="campos__cliente">
                         {camposCreador.map((campo) => (
-                            <div className='campo'>
+                            <div key={campo.label} className="campo">
                                 <label htmlFor={campo.label}>{campo.label}</label>
-                                <input type={campo.type} />
+                                <input type={campo.type} value={campo.value} onChange={campo.onChange} />
                             </div>
                         ))}
-
-                    </div>
+                    </section>
+                    <section className="campos__productos">
+                        {camposProducto.map((producto, index) => (
+                            <div key={index}>
+                                <h3>{producto.label}</h3>
+                                {producto.campos.map((campo) => (
+                                    <div key={campo.label} className="campo">
+                                        <label htmlFor={campo.label}>{campo.label}</label>
+                                        <input type={campo.type} />
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
+                    </section>
                 </div>
             ) : (
                 <></>
