db1d256c3dbf0b287df29bfa5c1883f06ac0544a
FranciscoC <123523494+CarpinchoRecu@users.noreply.github.com>
2023-11-22 13:53:16 -0300

    1.0.6
    haciendo ui de editar productos y clientes

--- tapon/src/renderer/src/components/Editar.jsx
+++ tapon/src/renderer/src/components/Editar.jsx
@@ -1,6 +1,21 @@
+import { useState } from 'react'
 import { FaRegArrowAltCircleLeft } from 'react-icons/fa'
+import { MdCancel } from 'react-icons/md'
 
 const Editar = ({ datosOriginal, idSeleccionado, setEditar, setTocarCliente }) => {
+    const [mostrarMenuProductoSeleccionado, setMostrarMenuProductoSeleccionado] = useState(false)
+    const [productoSeleccionado, setProductoSeleccionado] = useState(null)
+
+    const handleMostrarProductoSeleccionado = (producto) => {
+        setProductoSeleccionado(producto)
+        setMostrarMenuProductoSeleccionado(true)
+    }
+
+    const handleCerrarProductoSeleccionado = () => {
+        setProductoSeleccionado(null)
+        setMostrarMenuProductoSeleccionado(false)
+    }
+
     const handleCerrarEditor = () => {
         setEditar(false)
         setTocarCliente(false) // Cambia el estado de editar a false en Footer
@@ -12,18 +27,50 @@ const Editar = ({ datosOriginal, idSeleccionado, setEditar, setTocarCliente }) =
         return <p>Cliente no encontrado</p>
     }
 
-  // Encontrar todos los productos con el mismo nombre, localidad y dirección del cliente seleccionado
-  const productosCliente = datosOriginal.filter(
-    (cliente) =>
-      cliente.nombre.toLowerCase() === clienteSeleccionado.nombre.toLowerCase() &&
-      cliente.localidad === clienteSeleccionado.localidad &&
-      cliente.direccion === clienteSeleccionado.direccion
-  )
+    // Encontrar todos los productos con el mismo nombre, localidad y dirección del cliente seleccionado
+    const productosCliente = datosOriginal.filter(
+        (cliente) =>
+            cliente.nombre.toLowerCase() === clienteSeleccionado.nombre.toLowerCase() &&
+            cliente.localidad === clienteSeleccionado.localidad &&
+            cliente.direccion === clienteSeleccionado.direccion
+    )
+
+    const camposProductosSeleccionados = [
+        {
+            name: 'nombre_producto',
+            label: 'Nombre Producto',
+            type: 'text'
+        },
+        {
+            name: 'precio_producto',
+            label: 'Precio Producto',
+            placeholder: 'productoSeleccionado.precio_producto',
+            type: 'number'
+        },
+        {
+            name: 'cuotas_producto',
+            label: 'Cuotas',
+            placeholder: 'productoSeleccionado.cuotas_producto',
+            type: 'number'
+        },
+        {
+            name: 'cuotas_pagadas',
+            label: 'Cuotas Pagadas',
+            placeholder: 'productoSeleccionado.cuotas_pagadas',
+            type: 'number'
+        },
+        {
+            name: 'fecha_ultimo_pago',
+            label: 'Fecha Ultimo Pago',
+            placeholder: 'productoSeleccionado.fecha_ultimo_pago',
+            type: 'date'
+        }
+    ]
 
     return (
         <div className="editar">
             <div onClick={handleCerrarEditor} className="btn__volver">
-                <FaRegArrowAltCircleLeft/>
+                <FaRegArrowAltCircleLeft />
             </div>
             <table className="tabla__datos__cliente">
                 <thead>
@@ -34,11 +81,11 @@ const Editar = ({ datosOriginal, idSeleccionado, setEditar, setTocarCliente }) =
                     </tr>
                 </thead>
                 <tbody>
-                            <tr>
-                                <td>{clienteSeleccionado.nombre}</td>
-                                <td>{clienteSeleccionado.direccion}</td>
-                                <td>{clienteSeleccionado.localidad}</td>
-                            </tr>
+                    <tr>
+                        <td>{clienteSeleccionado.nombre}</td>
+                        <td>{clienteSeleccionado.direccion}</td>
+                        <td>{clienteSeleccionado.localidad}</td>
+                    </tr>
                 </tbody>
             </table>
             <table className="tabla__datos__productos">
@@ -57,13 +104,13 @@ const Editar = ({ datosOriginal, idSeleccionado, setEditar, setTocarCliente }) =
                 </thead>
                 <tbody>
                     {productosCliente.map((producto, index) => {
-                        const precioPorCuota = producto.precio_producto / producto.cuotas_producto;
-                        const totalPagado = producto.cuotas_pagadas * precioPorCuota;
-                        const faltaPagar = producto.precio_producto - totalPagado;
-                        const faltaCuotas = producto.cuotas_producto - producto.cuotas_pagadas;
+                        const precioPorCuota = producto.precio_producto / producto.cuotas_producto
+                        const totalPagado = producto.cuotas_pagadas * precioPorCuota
+                        const faltaPagar = producto.precio_producto - totalPagado
+                        const faltaCuotas = producto.cuotas_producto - producto.cuotas_pagadas
 
                         return (
-                            <tr key={index}>
+                            <tr key={index} onClick={() => handleMostrarProductoSeleccionado(producto)}>
                                 <td>{producto.nombre_producto}</td>
                                 <td>{producto.precio_producto}</td>
                                 <td>{producto.cuotas_producto}</td>
@@ -78,6 +125,26 @@ const Editar = ({ datosOriginal, idSeleccionado, setEditar, setTocarCliente }) =
                     })}
                 </tbody>
             </table>
+
+            {mostrarMenuProductoSeleccionado && productoSeleccionado && (
+                <div className="editor">
+                    <div onClick={handleCerrarProductoSeleccionado} className="cerrar__editor">
+                        <MdCancel />
+                    </div>
+                    <div className="contenedor__campo__editor">
+                        <div className="campos__editor">
+                            {camposProductosSeleccionados.map((campoSeleccionado, indexCampoSeleccionado) => (
+                                <input
+                                    key={indexCampoSeleccionado}
+                                    className="campo__editor"
+                                    placeholder={productoSeleccionado[campoSeleccionado.name]}
+                                    type={campoSeleccionado.type}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
