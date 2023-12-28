4f028d3d9519f56b78a4e8f82c0e3d98f307fc08
FranciscoC <123523494+CarpinchoRecu@users.noreply.github.com>
2023-12-28 15:15:48 -0300

    1.0.1

--- tapon/src/renderer/src/components/EdicionProductos/EliminarProducto.jsx
+++ tapon/src/renderer/src/components/EdicionProductos/EliminarProducto.jsx
@@ -1,20 +1,61 @@
-import { useContext } from 'react'
-import { IdContext } from '../../context/GeneralContext.jsx'
+import React, { useContext } from 'react'
+import { ProductosContext, IdContext } from '../../context/GeneralContext.jsx'
 import { useDatosContext } from '../../context/DatosContextFile.jsx'
-import { ClienteSeleccionado } from '../../utils/utilsClienteSeleccionado.jsx'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { ClienteSeleccionado } from '../../utils/utilsClienteSeleccionado.jsx'
 
 const EliminarProducto = () => {
-    // --------------------------------------------------------- //
-    const datosOriginal = useDatosContext()
-    const idSeleccionado = useContext(IdContext)
-    const clienteSeleccionado = ClienteSeleccionado(datosOriginal, idSeleccionado)
-    // --------------------------------------------------------- //
+  // --------------------------------------------------------- //
+  const productoSeleccionado = useContext(ProductosContext)
+   const datosOriginal = useDatosContext()
+   const idSeleccionado = useContext(IdContext)
+   // --------------------------------------------------------- //
+ 
+   // Encontrar el cliente correspondiente al idSeleccionado
+   const clienteSeleccionado = ClienteSeleccionado(datosOriginal, idSeleccionado)
+
+   const handleEliminar = async () => {
+    try {
+      const { nombre, localidad, direccion } = clienteSeleccionado
+      const resultado = await window.electronAPI.EliminarClienteSQLite(nombre, localidad, direccion)
+
+            // Mostrar mensaje de éxito
+            toast.success('Producto eliminado correctamente', {
+              position: 'top-right',
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: 'dark'
+            })
+    } catch (error) {
+            // Mostrar mensaje de error en caso de falla
+            console.log(error)
+            toast.error('Hubo un problema al eliminar al producto', {
+              position: 'top-right',
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: 'dark'
+            })
+    }
+   }
+
 
   return (
     <div className="eliminar__producto">
       <h2>Eliminar Producto</h2>
+      <p>{productoSeleccionado.nombre_producto}</p>
+      <p>{productoSeleccionado.precio_producto}</p>
+      <div className="btn__eliminar" onClick={handleEliminar}>
+          <p>Eliminar</p>
+        </div>
     </div>
   )
 }
